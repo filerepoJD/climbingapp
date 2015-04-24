468e7b5b399fd2718feaa11314b82676c4006bb8
Joachim D'eigens <joachim.deigens@telenet.be>
2015-04-24 17:25:44 +0200

    update dbconfig

--- include/Config.php
+++ include/Config.php
@@ -4,7 +4,7 @@
  */
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.13.89.2:3306');
 define('DB_NAME', 'climbing_app');
 
 define('USER_CREATED_SUCCESSFULLY', 0);
