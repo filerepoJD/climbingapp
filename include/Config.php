462b76dd89c246db47fb2067732c9198e4a99aed
Joachim D'eigens <joachim.deigens@telenet.be>
2015-04-25 13:32:19 +0200

    require edit

--- include/Config.php
+++ include/Config.php
@@ -2,10 +2,22 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'adminSuCeKe3');
-define('DB_PASSWORD', 'jVqDkpPA3bnP');
-define('DB_HOST', '127.13.89.2:3306');
-define('DB_NAME', 'climbingapp');
+
+define('OPENSHIFT_MYSQL_DB_USERNAME', 'adminSuCeKe3');
+define('OPENSHIFT_MYSQL_DB_PASSWORD', 'jVqDkpPA3bnP');
+define('OPENSHIFT_MYSQL_DB_HOST', '127.13.89.2');
+define('OPENSHIFT_MYSQL_DB_PORT', '3306');
+define('OPENSHIFT_GEAR_NAME', 'climbingapp');
+
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_127.13.89.2'));
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_3306'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_adminSuCeKe3'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_jVqDkpPA3bnP'));
+define('DB_NAME', getenv('OPENSHIFT_climbingapp'));
+
+
+
+
 
 define('USER_CREATED_SUCCESSFULLY', 0);
 define('USER_CREATE_FAILED', 1);
